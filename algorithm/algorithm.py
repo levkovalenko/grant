8841187ff962c54cd94789645eae87da6312e863
korwin <levozavr@mail.ru>
2018-10-22 09:58:58 +0300

    incapsulation

--- algorithm/algorithm.py
+++ algorithm/algorithm.py
@@ -55,5 +55,14 @@ class BaseMethod(object):
             dist_way += self.distance_matrix[way[i],way[i+1]]
         return dist_way
 
+    def get_matrix(self):
+        return self.distance_matrix
+
+    def put_matrix(self, matrix):
+        self.distance_matrix = matrix
+
+    def get_ways(self):
+        return self.__ways
+
 a = BaseMethod(4)
 print(a.start())
