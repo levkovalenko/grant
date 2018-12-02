e5436e5d7016ab4c93b36cc615f79b9e7ea6f1f7
korwin <levozavr@mail.ru>
2018-12-02 23:58:14 +0300

    add crossover

--- algorithm/tour.py
+++ algorithm/tour.py
@@ -1,3 +1,5 @@
+from random import randint
+
 class Tour(object):
     def __init__(self, tour=list(), good_view=True, dist_matrix=None):
         if good_view:
@@ -5,7 +7,7 @@ class Tour(object):
         else:
             self.tour = tour.append(tour[0])
         if dist_matrix:
-            self.__dist = self.calc_distance(dist_matrix)
+            self.calc_distance(dist_matrix)
         else:
             self.__dist = -1
 
@@ -14,7 +16,6 @@ class Tour(object):
         for i in range(len(self.tour) - 1):
             dist_way += matrix[self.tour[i], self.tour[i + 1]]
         self.__dist = dist_way
-        return dist_way
 
     @property
     def dist(self):
@@ -25,3 +26,24 @@ class Tour(object):
         for town in self.tour:
             string += f"{town} "
         return string
+
+    @classmethod
+    def crossover(cls, p_tour1, p_tour2, cross_len=3):
+        child = p_tour1
+        number_of_towns = len(p_tour1)
+        pos = randint(1,number_of_towns-cross_len)
+        new_towns = set()
+        old_towns = set()
+        for i in range(pos, cross_len+pos):
+            old_towns.add(child[i])
+            new_towns.add(p_tour2[i])
+            child[i] = p_tour2[i]
+        towns_to_add = old_towns - new_towns
+        towns_to_delet = new_towns - old_towns
+        for town_add, town_del in list(zip(list(towns_to_add), list(towns_to_delet))):
+            child[child.index(town_del)] = town_add
+        return child
+
+
+if __name__ == "__main__":
+    print(Tour.crossover([1, 2, 3, 4, 5, 6, 7, 8], [5,6,8,3,7,2,4,1],4))
